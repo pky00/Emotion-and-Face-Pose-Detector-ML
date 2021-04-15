d177c7a46bd52e106963b040c9d596e5ec67af34
pky00 <44399203+pky00@users.noreply.github.com>
2021-04-15 20:50:37 +0300

    made FaceExtractor

---
+++ FaceExtractor.py
@@ -0,0 +1,33 @@
+import cv2
+import sys
+
+def fixImage(imagePath):
+
+	image = cv2.imread(imagePath)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+	faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+	
+	faces = faceCascade.detectMultiScale(
+		gray,
+		scaleFactor=1.3,
+		minNeighbors=5,
+		minSize=(48, 48)
+	)
+
+	for (x, y, w, h) in faces:
+		gray_image = gray[y:y + h, x:x + w]
+		resized_image = cv2.resize(gray_image, (48,48))
+		cv2.imwrite("NEW_"+str(imagePath), resized_image)
+	
+	return
+
+
+def resize(imagePath):
+	image = cv2.imread(imagePath)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	resized_image = cv2.resize(gray, (48,48))
+	cv2.imwrite("NEW_"+str(imagePath), resized_image)
+	return
+
+fixImage(sys.argv[1])
